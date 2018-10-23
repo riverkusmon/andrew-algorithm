182352bf7001ab94b0a0e18172ffb028a808ada3
Andrew <andrewgardner@live.com>
2018-10-23 12:06:49 -0600

    Initial commit

---
+++ andrew-algorithm/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  andrew-algorithm
+//
+//  Created by Gardner, Andrew on 10/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ andrew-algorithm/Model/algorithm-model.swift
@@ -0,0 +1,9 @@
+//
+//  algorithm-model.swift
+//  andrew-algorithm
+//
+//  Created by Gardner, Andrew on 10/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
